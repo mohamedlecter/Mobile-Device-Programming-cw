8cd004851162939b723cca39bc11e434336c0e77
Mohamed Abubaker <67129352+mohamedlecter@users.noreply.github.com>
2023-11-01 17:00:57 +0800

    login page

--- app/src/main/java/com/example/cw/AuthenticationManager.java
+++ app/src/main/java/com/example/cw/AuthenticationManager.java
@@ -7,20 +7,20 @@ public class AuthenticationManager {
     // Simulated user database
     private static final Map<String, String> userDatabase = new HashMap<>();
 
-    public static boolean isValidLogin(String username, String password) {
-        // Simulated logic: Check if the username and password match a stored user
-        String storedPassword = userDatabase.get(username);
+    public static boolean isValidLogin(String email, String password) {
+        // Simulated logic: Check if the email and password match a stored user
+        String storedPassword = userDatabase.get(email);
         return storedPassword != null && storedPassword.equals(password);
     }
 
-    public static boolean isValidSignUp(String username, String email, String password) {
+    public static boolean isValidSignUp(String email, String username, String password) {
         // Simulated logic: Check if the username is not already taken
-        if (userDatabase.containsKey(username)) {
-            return false; // Username is already in use
+        if (userDatabase.containsKey(email)) {
+            return false; // email is already in use
         }
 
         // Simulated logic: Store the new user's data
-        userDatabase.put(username, password);
+        userDatabase.put(email, password);
 
         return true;
     }
