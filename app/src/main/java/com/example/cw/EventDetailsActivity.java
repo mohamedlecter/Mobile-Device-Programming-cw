bea27ef3cf4bb67673e5dba884a96da3816565e3
Mohamed Abubaker <67129352+mohamedlecter@users.noreply.github.com>
2023-11-26 18:56:25 +0800

    display event data

--- app/src/main/java/com/example/cw/EventDetailsActivity.java
+++ app/src/main/java/com/example/cw/EventDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.cw.model.Event;
@@ -20,11 +22,33 @@ public class EventDetailsActivity extends AppCompatActivity {
         if (intent != null && intent.hasExtra("event")) {
             Event event = (Event) intent.getSerializableExtra("event");
 
-            // Retrieve TextView from layout
+            // Retrieve TextViews from layout
             TextView eventNameTextView = findViewById(R.id.eventNameTextView);
+            TextView eventDateTextView = findViewById(R.id.eventDateTextView);
+            TextView eventLocationTextView = findViewById(R.id.eventLocationTextView);
+            TextView eventOrganizerNameTextView = findViewById(R.id.eventOrganizerNameTextView);
+            TextView eventDescTextView = findViewById(R.id.eventDescTextView);
 
-            // Set event name in the TextView
+
+            // Set event details in the TextViews
             eventNameTextView.setText(event.getTitle());
+            eventDateTextView.setText(event.getDate());
+            eventLocationTextView.setText(event.getLocation());
+//            eventOrganizerNameTextView.setText("Organizer: " + event.getOrganizer());
+            eventDescTextView.setText(event.getDescription());
+
+
+            // Button for redirecting to the home page
+            Button backButton = findViewById(R.id.button3);
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Create an intent to go back to the home page
+                    Intent homeIntent = new Intent(EventDetailsActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                }
+            });
         }
-    }
+
+        }
 }
